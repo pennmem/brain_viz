073c468729edb78b61ae4cc693c17106da235c36
Zach Duey zachduey@gmail.com <zduey@rhino2.rhino.psych.upenn.edu>
2017-07-03 16:12:05 -0400

    Initial files. Functional but flawed implementation.

---
+++ convert.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+echo $6
+
+CODEDIR=$(pwd) # current/code directory
+SUBJECT=$1
+SUBJECT_NUM=$2
+BASEDIR=$3
+CORTEXDIR=$4
+CONTACTDIR=$5
+OUTDIR=$6
+
+mkdir $CORTEXDIR
+
+cp $BASEDIR/surf/lh.pial $CORTEXDIR/
+cp $BASEDIR/surf/rh.pial $CORTEXDIR/
+cp $BASEDIR/label/lh.aparc.annot $CORTEXDIR/
+cp $BASEDIR/label/rh.aparc.annot $CORTEXDIR/
+
+mris_convert $CORTEXDIR/lh.pial $CORTEXDIR/lh.pial.asc
+mris_convert $CORTEXDIR/rh.pial $CORTEXDIR/rh.pial.asc
+
+mv $CORTEXDIR/lh.pial.asc $CORTEXDIR/lh.pial.srf
+mv $CORTEXDIR/rh.pial.asc $CORTEXDIR/rh.pial.srf
+
+srf2obj $CORTEXDIR/lh.pial.srf > $CORTEXDIR/lh.pial.obj
+srf2obj $CORTEXDIR/rh.pial.srf > $CORTEXDIR/rh.pial.obj
+
+cd $CORTEXDIR/
+matlab -r "annot2dpv lh.aparc.annot lh.aparc.annot.dpv;annot2dpv rh.aparc.annot rh.aparc.annot.dpv;splitsrf lh.pial.srf lh.aparc.annot.dpv lh.pial_roi;splitsrf rh.pial.srf rh.aparc.annot.dpv rh.pial_roi;exit;"
+
+cd $CODEDIR
+
+# Rename srf files to something human readable
+srf2obj $CORTEXDIR/lh.pial_roi.0001.srf > $CORTEXDIR/lh.Unmeasured.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0002.srf > $CORTEXDIR/lh.BanksSuperiorTemporal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0003.srf > $CORTEXDIR/lh.CACingulate.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0004.srf > $CORTEXDIR/lh.MiddleFrontalCaudal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0005.srf > $CORTEXDIR/lh.Cuneus.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0006.srf > $CORTEXDIR/lh.Entorhinal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0007.srf > $CORTEXDIR/lh.Fusiform.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0008.srf > $CORTEXDIR/lh.InferiorParietal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0009.srf > $CORTEXDIR/lh.InferiorTemporal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0010.srf > $CORTEXDIR/lh.Isthmus.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0011.srf > $CORTEXDIR/lh.LateralOccipital.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0012.srf > $CORTEXDIR/lh.OrbitalFrontal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0013.srf > $CORTEXDIR/lh.Lingual.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0014.srf > $CORTEXDIR/lh.MedialOrbitalFrontal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0015.srf > $CORTEXDIR/lh.MiddleTemporal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0016.srf > $CORTEXDIR/lh.Parahippocampal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0017.srf > $CORTEXDIR/lh.ParacentralLobule.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0018.srf > $CORTEXDIR/lh.InfFrontalParsOpercularis.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0019.srf > $CORTEXDIR/lh.InfFrontalParsOrbitalis.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0020.srf > $CORTEXDIR/lh.InfFrontalParsTriangularis.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0021.srf > $CORTEXDIR/lh.Pericalcarine.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0022.srf > $CORTEXDIR/lh.Post-Central.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0023.srf > $CORTEXDIR/lh.PosteriorCingulate.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0024.srf > $CORTEXDIR/lh.Pre-Central.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0025.srf > $CORTEXDIR/lh.PreCuneus.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0026.srf > $CORTEXDIR/lh.RACingulate.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0027.srf > $CORTEXDIR/lh.MiddleFrontalRostral.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0028.srf > $CORTEXDIR/lh.SuperiorFrontal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0029.srf > $CORTEXDIR/lh.SuperiorParietal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0030.srf > $CORTEXDIR/lh.SuperiorTemporal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0031.srf > $CORTEXDIR/lh.Supra-Marginal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0032.srf > $CORTEXDIR/lh.FrontalPole.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0033.srf > $CORTEXDIR/lh.TemporalPole.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0034.srf > $CORTEXDIR/lh.TransverseTemporal.obj
+srf2obj $CORTEXDIR/lh.pial_roi.0035.srf > $CORTEXDIR/lh.Insula.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0001.srf > $CORTEXDIR/rh.Unmeasured.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0002.srf > $CORTEXDIR/rh.BanksSuperiorTemporal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0003.srf > $CORTEXDIR/rh.CACingulate.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0004.srf > $CORTEXDIR/rh.MiddleFrontalCaudal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0005.srf > $CORTEXDIR/rh.Cuneus.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0006.srf > $CORTEXDIR/rh.Entorhinal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0007.srf > $CORTEXDIR/rh.Fusiform.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0008.srf > $CORTEXDIR/rh.InferiorParietal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0009.srf > $CORTEXDIR/rh.InferiorTemporal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0010.srf > $CORTEXDIR/rh.Isthmus.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0011.srf > $CORTEXDIR/rh.LateralOccipital.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0012.srf > $CORTEXDIR/rh.OrbitalFrontal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0013.srf > $CORTEXDIR/rh.Lingual.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0014.srf > $CORTEXDIR/rh.MedialOrbitalFrontal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0015.srf > $CORTEXDIR/rh.MiddleTemporal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0016.srf > $CORTEXDIR/rh.Parahippocampal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0017.srf > $CORTEXDIR/rh.ParacentralLobule.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0018.srf > $CORTEXDIR/rh.InfFrontalParsOpercularis.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0019.srf > $CORTEXDIR/rh.InfFrontalParsOrbitalis.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0020.srf > $CORTEXDIR/rh.InfFrontalParsTriangularis.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0021.srf > $CORTEXDIR/rh.Pericalcarine.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0022.srf > $CORTEXDIR/rh.Post-Central.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0023.srf > $CORTEXDIR/rh.PosteriorCingulate.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0024.srf > $CORTEXDIR/rh.Pre-Central.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0025.srf > $CORTEXDIR/rh.PreCuneus.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0026.srf > $CORTEXDIR/rh.RACingulate.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0027.srf > $CORTEXDIR/rh.MiddleFrontalRostral.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0028.srf > $CORTEXDIR/rh.SuperiorFrontal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0029.srf > $CORTEXDIR/rh.SuperiorParietal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0030.srf > $CORTEXDIR/rh.SuperiorTemporal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0031.srf > $CORTEXDIR/rh.Supra-Marginal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0032.srf > $CORTEXDIR/rh.FrontalPole.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0033.srf > $CORTEXDIR/rh.TemporalPole.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0034.srf > $CORTEXDIR/rh.TransverseTemporal.obj
+srf2obj $CORTEXDIR/rh.pial_roi.0035.srf > $CORTEXDIR/rh.Insula.obj
+
+# Generate coordinatees for blender
+cd $CONTACTDIR/
+matlab -r "coords4blender('$SUBJECT', '$CONTACTDIR'); exit;"
+
+# Make a copy of the template for blender specific to the patient
+mkdir $OUTDIR
+mkdir $OUTDIR/axial
+mkdir $OUTDIR/coronal
+
+cd $CODEDIR
+cp  iEEG_surface_template/* $OUTDIR/
+cp $CONTACTDIR/monopolar_names.txt $OUTDIR/
+cp $CONTACTDIR/bipolar_names.txt $OUTDIR/
+cp $CONTACTDIR/monopolar_start_names.txt $OUTDIR/
+
+# Generate the blender scene .blend and .json files
+# This is not in brain_viz.py because it needs to be run with the python runtime
+# that is bundled with blender, otherwise there is no access to the bpy library
+~/blender/blender -b empty.blend -b --python create_scene.py -- $SUBJECT $SUBJECT_NUM $CORTEXDIR $CONTACTDIR $OUTDIR
+
+# Copy over coordinates
+## TODO: Remove this hardcoding
+cp /data10/RAM/subjects/$SUBJECT/tal/VOX_coords_mother_dykstra.txt $OUTDIR/
+cp /data10/RAM/subjects/$SUBJECT/tal/VOX_coords_mother_dykstra_bipolar.txt $OUTDIR/
+
+
+exit
---
+++ coords4blender.m
@@ -0,0 +1,101 @@
+function coords4blender(subj, basedir)
+%blender_coords = coords4blender(subj)
+% provides monopolar and bipolar  coordinates from the talStruct to use for
+% visualization in blender. Also gives original starting coordinates.
+%
+% subj ............... The subject being analyzed.
+
+% -JMS (October 2016)
+% joel.stein@uphs.upenn.edu
+
+%Get the monopolar data from talStruct
+try
+   talStruct_mono = getBipolarSubjElecs(subj,1);
+catch
+    disp(sprintf('%s: No talStruct',subj));
+    return;
+end
+talStruct_mono = getBipolarSubjElecs(subj,0);
+monopolars = {talStruct_mono.tagName};
+monoTypes = {talStruct_mono.eType};
+monoSurf = [talStruct_mono.indivSurf];
+
+%Get the bipolar data from talStruct
+talStruct = getBipolarSubjElecs(subj,1);
+bipolars = {talStruct.tagName};
+bipoTypes = {talStruct.eType};
+bipoSurf = [talStruct.indivSurf];
+
+%Get the original starting monopolar coordinates
+if isfield(monoSurf,'x')
+    start_coords = 0.02*double([[monoSurf.x]', [monoSurf.y]', [monoSurf.z]']);
+else
+    disp(sprintf('%s: Could not get starting coordinates',subj));
+    return;
+end
+
+%Check if surface electrodes are present based on eType in talStruct
+if ~any(strcmp(monoTypes,'S')) & ~any(strcmp(monoTypes,'G'))
+    disp(sprintf('%s: No surface electrodes',subj));
+    surf_coords = 0.02*double([[monoSurf.x]', [monoSurf.y]', [monoSurf.z]']);
+    surf_coords_bipolar = 0.02*double([[bipoSurf.x]', [bipoSurf.y]', [bipoSurf.z]']);
+    %return;
+else
+
+%Check if Dykstra coordinates are present
+if isfield(monoSurf,'x_Dykstra')
+    surf_coords = 0.02*double([[monoSurf.x_Dykstra]', [monoSurf.y_Dykstra]', [monoSurf.z_Dykstra]']);
+else
+    disp(sprintf('%s: Surface electrodes but no Dykstra coords',subj));
+    return;
+end
+
+surf_coords_bipolar = 0.02*double([[bipoSurf.x_Dykstra]', [bipoSurf.y_Dykstra]', [bipoSurf.z_Dykstra]']);
+
+end
+
+%write to the specified or default outfile for monopolar starting coordinates
+outfile_starting = fullfile(basedir, 'monopolar_start_blender.txt')
+fid = fopen(outfile_starting,'w');
+for elec_i=1:size(surf_coords,1)
+    fprintf(fid, '%s\t%d\t%d\t%d\t%s\n', strcat('o', monopolars{1,elec_i}), start_coords(elec_i,1), ...
+    start_coords(elec_i,2), start_coords(elec_i,3), monoTypes{1,elec_i});
+end
+
+%write to the specified or default outfile for monopolars
+outfile = fullfile(basedir, 'monopolar_blender.txt')
+fid = fopen(outfile,'w');
+for elec_i=1:size(surf_coords,1)
+    fprintf(fid, '%s\t%d\t%d\t%d\t%s\n', monopolars{1,elec_i}, surf_coords(elec_i,1), ...
+    surf_coords(elec_i,2), surf_coords(elec_i,3), monoTypes{1,elec_i});
+end
+
+%write to the specified or default outfile for bipolars
+outfile_bipolar = fullfile(basedir, 'bipolar_blender.txt')
+fid = fopen(outfile_bipolar,'w');
+for elec_i=1:size(surf_coords_bipolar,1)
+    fprintf(fid, '%s\t%d\t%d\t%d\t%s\n', bipolars{1,elec_i}, surf_coords_bipolar(elec_i,1), ...
+    surf_coords_bipolar(elec_i,2), surf_coords_bipolar(elec_i,3), bipoTypes{1,elec_i});
+end
+
+%write the names of the monopolar starting coordinates
+outfile = fullfile(basedir, 'monopolar_start_names.txt')
+fid = fopen(outfile,'w');
+for elec_i=1:size(surf_coords,1)
+    fprintf(fid, '"%s",', strcat('o', monopolars{1,elec_i}));
+end
+
+%write the names of the monopolars
+outfile = fullfile(basedir, 'monopolar_names.txt')
+fid = fopen(outfile,'w');
+for elec_i=1:size(surf_coords,1)
+    fprintf(fid, '"%s",', monopolars{1,elec_i});
+end
+
+%write the names of the bipolars
+outfile_bipolar = fullfile(basedir, 'bipolar_names.txt')
+fid = fopen(outfile_bipolar,'w');
+for elec_i=1:size(surf_coords_bipolar,1)
+    fprintf(fid, '"%s",', bipolars{1,elec_i});
+end
+
