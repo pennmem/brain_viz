e5e0155f06e7997a44065c4a4d4d86d2446e4433
Zach Duey <zachduey@gmail.com>
2017-08-18 09:19:05 -0400

    Fixed group/num extraction to work with groups starting with numbers (ex: 12LAD)

--- src/coords4blender.py
+++ src/coords4blender.py
@@ -18,7 +18,10 @@ def save_coords_for_blender(subject, outdir):
     mono_structs = mono_reader.read()
 
     mono_start_df = get_coords(mono_structs, 'monopolar')
+    mono_start_df["contact_name"] = "o" + mono_start_df["contact_name"]
+
     mono_adj_df = get_coords(mono_structs, 'monopolar', atlas='Dykstra')
+
     bipo_adj_df = get_coords(bipol_structs, 'bipolar', atlas='Dykstra')
 
     all_coord_df = pd.concat([mono_start_df, mono_adj_df, bipo_adj_df])
@@ -26,6 +29,8 @@ def save_coords_for_blender(subject, outdir):
     for col in ['x', 'y', 'z']:
         all_coord_df[col] = 0.02 * all_coord_df[col]
 
+    all_coord_df = add_orientation_contact_for_depth_electrodes(all_coord_df)
+    all_coord_df = all_coord_df[["contact_name", "contact_type", "x", "y", "z", "atlas", "orient_to"]]
     all_coord_df.to_csv(outdir + "/electrode_coordinates.csv", index=False)
 
     return
@@ -55,3 +60,64 @@ def get_coords(talStruct, elec_type, atlas=None):
 
     return df
 
+def extract_group_num(contact_name):
+    found_start = False
+    if contact_name.find('-') != -1:
+        contact_name = contact_name[0 : contact_name.find('-')]
+
+    # Iterate over name from the end until integer conversion fails
+    name_length = len(contact_name)
+    found_start = False
+    for i in range(1 , name_length + 1):
+        try:
+            int_rep = int(contact_name[name_length - i])
+            continue
+        except ValueError as e:
+            found_start = True
+            start_index = name_length - i
+        if found_start:
+            break
+    group = contact_name[:start_index + 1]
+    num = int(contact_name[start_index + 1:])
+    return group, num
+
+def add_orientation_contact_for_depth_electrodes(all_coord_df):
+    all_coord_df["group"] = all_coord_df["contact_name"].apply(lambda x: extract_group_num(x)[0])
+    all_coord_df["num"] = all_coord_df["contact_name"].apply(lambda x: extract_group_num(x)[1])
+    max_num_df = (all_coord_df.groupby(by=["group"])
+                              .agg({"num":max})
+                              .reset_index()
+                              .rename(columns={"num":"max_num"})
+                 )
+    all_coord_df = all_coord_df.merge(max_num_df)
+    all_coord_df["depth"] = (all_coord_df["contact_type"].str.find("D") != -1)
+    all_coord_df["bipolar"] = (all_coord_df["contact_name"].str.find('-') != -1)
+    all_coord_df["num_str"] = all_coord_df["num"].astype(str)
+    all_coord_df["num_plus_one"] = all_coord_df["num"].apply(lambda x: str(x + 1))
+    all_coord_df["num_plus_two"] = all_coord_df["num"].apply(lambda x: str(x + 2))
+
+    all_coord_df["bipolar_orient_to"] = (all_coord_df["group"] +
+                                         all_coord_df["num_plus_one"] +
+                                         "-" +
+                                         all_coord_df["group"] +
+                                         all_coord_df["num_plus_two"])
+    all_coord_df["monopolar_orient_to"] = (all_coord_df["group"] +
+                                           all_coord_df["num_plus_one"])
+
+    all_coord_df['orient_to'] = None
+    all_coord_df.loc[(all_coord_df["bipolar"] == True) &
+                     (all_coord_df["depth"] == True) &
+                     (all_coord_df["num"] < all_coord_df["max_num"] - 1),
+                     "orient_to"] = all_coord_df.loc[(all_coord_df["bipolar"] == True) &
+                                                     (all_coord_df["depth"] == True) &
+                                                     (all_coord_df["num"] < all_coord_df["max_num"] - 1),
+                                                     "bipolar_orient_to"]
+    all_coord_df.loc[(all_coord_df["bipolar"] == False) &
+                     (all_coord_df["depth"] == True) &
+                     (all_coord_df["num"] < all_coord_df["max_num"]),
+                     "orient_to"] = all_coord_df.loc[(all_coord_df["bipolar"] == False) &
+                                                     (all_coord_df["depth"] == True) &
+                                                     (all_coord_df["num"] < all_coord_df["max_num"]),"monopolar_orient_to"]
+
+
+    return all_coord_df
--- src/test_coords4blender.py
+++ src/test_coords4blender.py
@@ -2,7 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 import subprocess
-from coords4blender import save_coords_for_blender
+from coords4blender import save_coords_for_blender, extract_group_num
+
 
 def test_matlab_python_match():
     subject = "R1291M_1"
@@ -27,4 +28,14 @@ def test_matlab_python_match():
     assert np.allclose(python_mono_coords, matlab_mono_coords, atol=1e-1)
     assert np.allclose(python_bipol_coords, matlab_bipol_coords, atol=1e-1)
 
+    return
+
+def test_extract_group_num():
+    assert extract_group_num("LAD1") == ("LAD", 1)
+    assert extract_group_num("LAD41") == ("LAD", 41)
+    assert extract_group_num("RHLD1-RHLD2") == ("RHLD", 1)
+    assert extract_group_num("HTYX28-HTYX28") == ("HTYX", 28)
+    assert extract_group_num("1TAL2") == ("1TAL", 2)
+    assert extract_group_num("23TAL9") == ("23TAL", 9)
+    assert extract_group_num("11ABC8-11ABC9") == ("11ABC", 8)
     return
