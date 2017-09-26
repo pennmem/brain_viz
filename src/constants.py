5a1a4e60e96a15f4dc85f2b0267128d9a1b991d7
Zach Duey <zachduey@gmail.com>
2017-09-26 14:58:48 -0400

    Moved constants to module

--- src/constants.py
+++
@@ -1,7 +0,0 @@
-CH2 = "~sudas/DARPA/ch2.nii.gz"
-RDIR = "~sudas/bin/localization/template_to_NickOasis"
-GENERIC_AFFINE_TRANSFORM_FILE = RDIR + "/ch22t0GenericAffine.mat"
-WARP_FILE = RDIR + "/ch22t1Warp.nii.gz"
-
-# List of subjects to ignore for prior stim locations
-SUBJECT_BLACKLIST = ["R1027J"]
