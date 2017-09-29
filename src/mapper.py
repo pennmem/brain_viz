b2eaff27394be398846d49ae1ed504677d464475
Zach Duey <zachduey@gmail.com>
2017-09-29 12:30:04 -0400

    Added R1018P to blacklist

--- src/mapper.py
+++ src/mapper.py
@@ -18,7 +18,7 @@ GENERIC_AFFINE_TRANSFORM_FILE = RDIR + "/ch22t0GenericAffine.mat"
 WARP_FILE = RDIR + "/ch22t1Warp.nii.gz"
 
 # List of subjects to ignore for prior stim locations
-SUBJECT_BLACKLIST = ["R1027J"]
+SUBJECT_BLACKLIST = ["R1018P", "R1027J"]
 
 
 def build_prior_stim_location_mapping(subject, basedir, imagedir):
