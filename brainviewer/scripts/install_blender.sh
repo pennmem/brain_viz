9b33cae9a8cd63110cdc47131ef6450b5734963e
Zach Duey <zachduey@gmail.com>
2018-06-11 11:50:42 -0400

    updated blender installation script

--- brainviewer/scripts/install_blender.sh
+++ brainviewer/scripts/install_blender.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 wget http://mirror.cs.umn.edu/blender.org/release/Blender2.78/blender-2.78c-linux-glibc219-x86_64.tar.bz2 -P ~/Downloads
-
 wget https://www.blend4web.com/pub/blend4web_ce_17_06.zip -P ~/Downloads
 
-tar -xzf ~/Downloads/blender-2.78c-linux-glibc219-x86_64.tar.bz2 --directory ~/bin/blender/
-
-mkdir ~/bin/blend4web/
-unzip ~/Downloads/blend4web_ce_17_06.zip -d ~/bin/blend4web/
+tar -xzf ~/Downloads/blender-2.78c-linux-glibc219-x86_64.tar.bz2 --directory ~/bin/
+mv ~/bin/blender-2.78c-linux-glibc219-x86_64.tar.bz2 ~/bin/blender/
+unzip ~/Downloads/blend4web_ce_17_06.zip -d ~/bin/
+mv ~/bin/blend4web_ce_17_06/ ~/bin/blend4web
 
 # Run setup_scripts.py with blender to initialize scripts directory within blender
 ~/blender/blender -b --python setup_scripts.py
