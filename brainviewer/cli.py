df40aa0e74a988294fa75f0af3173c303ebbe7f0
Zach Duey <zachduey@gmail.com>
2018-06-14 18:16:14 -0400

    use different default caching directory

--- brainviewer/cli.py
+++ brainviewer/cli.py
@@ -1,6 +1,10 @@
+import os
 from argparse import ArgumentParser
 from brainviewer.pipeline import generate_subject_brain, generate_average_brain
 from cml_pipelines.wrapper import memory
+
+
+default_cachedir = os.path.expanduser(os.path.join('~', '.brainviewer', 'cache'))
 
 
 def build_average_brain_visualization(input_args=None):
@@ -11,6 +15,7 @@ def build_average_brain_visualization(input_args=None):
                         help='Overwrite blender files if they exist')
 
     args = parser.parse_args(input_args)
+    memory.cachedir = default_cachedir
     generate_average_brain(blender=args.blender, force_rerun=args.force)
     memory.clear()
 
@@ -26,6 +31,7 @@ def build_subject_brain_visualization(input_args=None):
                         help='Overwrite blender files if they exist')
 
     args = parser.parse_args(input_args)
+    memory.cachedir = default_cachedir
     generate_subject_brain(args.subject, args.localization,
                            blender=args.blender, force_rerun=args.force)
     memory.clear()
