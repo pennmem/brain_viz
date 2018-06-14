1284a1a199df512b175b1df5fc0652b091e34a1c
Zach Duey <zachduey@gmail.com>
2018-06-14 17:03:38 -0400

    minor updates to mapper test script

--- brainviewer/tests/test_mapper.py
+++ brainviewer/tests/test_mapper.py
@@ -22,11 +22,11 @@ class TestMapper:
                               imagedir="imaging/autoloc/")
         cls.subject_id = "R1291M_1"
 
-    # @classmethod
-    # def teardown_class(cls):
-    #     if os.path.exists(cls.paths.workdir):
-    #         shutil.rmtree(cls.paths.workdir, ignore_errors=True)
-    #     return
+    @classmethod
+    def teardown_class(cls):
+         if os.path.exists(cls.paths.workdir):
+             shutil.rmtree(cls.paths.workdir, ignore_errors=True)
+         return
 
     def test_initialize(self):
         mapper.initialize(self.subject_id, self.paths.workdir + "/",
@@ -42,13 +42,13 @@ class TestMapper:
                                        "_stimdeltarec_target_CT.nii.gz"]))
 
     def test_get_orig_mat(self):
-        norig_matrix = mapper.get_orig_mat(self.paths.base, "/vox2ras")
+        norig_matrix = mapper.get_orig_mat(self.paths.base, "vox2ras")
         assert np.shape(norig_matrix) == (4, 4)
 
         norig_comparison_matrix = np.loadtxt(self.paths.baselinedir + "/Norig.txt")
         assert np.allclose(norig_matrix, norig_comparison_matrix)
 
-        torig_matrix = mapper.get_orig_mat(self.paths.base, "/vox2ras-tkr")
+        torig_matrix = mapper.get_orig_mat(self.paths.base, "vox2ras-tkr")
         assert np.shape(torig_matrix) == (4, 4)
 
     def test_generate_generic_ras_file(self):
@@ -68,30 +68,28 @@ class TestMapper:
         mni_df = mapper.load_orig_mni_coords(self.subject_id)
         assert len(mni_df) > 0
 
-    # def test_load_adj_mni_coords(self):
-    #     subject = "R1216E"
-    #     basedir, workdir, baselinedir, imagedir = setup_directories(subject)
-    #     mni_df = mapper.load_adj_mni_coords(subject)
-    #     assert len(mni_df) > 0
-    #
-    #     subject = "R1027J"
-    #     basedir, workdir, baselinedir, imagedir = setup_directories(subject)
-    #     mni_df = mapper.load_adj_mni_coords(subject)
-    #     assert (mni_df is None) # no adjusted coordinates (depths only)
+    @pytest.mark.rhino
+    def test_load_adj_mni_coords(self):
+        subject = "R1216E"
+        mni_df = mapper.load_adj_mni_coords(subject)
+        assert len(mni_df) > 0
 
-    # def test_get_mni_coords(self):
-    #     # R1291M_1 does not have the correct updated mni file, so use a subject
-    #     # who does have one
-    #     subject = "R1216E"
-    #     basedir, workdir, baselinedir, imagedir = setup_directories(subject)
-    #     mni_df = mapper.get_mni_coords(subject)
-    #     assert len(mni_df) > 0
-    #
-    #     # Unajusted coordinates only. Should still return data
-    #     subject = "R1027J"
-    #     basedir, workdir, baselinedir, imagedir = setup_directories(subject)
-    #     mni_df = mapper.get_mni_coords(subject)
-    #     assert len(mni_df) > 0
+        subject = "R1027J"
+        mni_df = mapper.load_adj_mni_coords(subject)
+        assert (mni_df is None) # no adjusted coordinates (depths only)
+
+    @pytest.mark.rhino
+    def test_get_mni_coords(self):
+        # R1291M_1 does not have the correct updated mni file, so use a subject
+        # who does have one
+        subject = "R1216E"
+        mni_df = mapper.get_mni_coords(subject)
+        assert len(mni_df) > 0
+
+        # Unajusted coordinates only. Should still return data
+        subject = "R1027J"
+        mni_df = mapper.get_mni_coords(subject)
+        assert len(mni_df) > 0
 
     @pytest.mark.rhino
     @pytest.mark.parametrize("stim_subject,bipolar_contact", [
@@ -137,8 +135,8 @@ class TestMapper:
 
     def test_get_fs_vector(self):
         stim_subject = "R1001P"
-        norig = mapper.get_orig_mat(self.paths.basedir, "/vox2ras")
-        torig = mapper.get_orig_mat(self.paths.basedir, "/vox2ras-tkr")
+        norig = mapper.get_orig_mat(self.paths.base, "vox2ras")
+        torig = mapper.get_orig_mat(self.paths.base, "vox2ras-tkr")
 
         fs_vector = mapper.get_fs_vector(self.paths.workdir + "/",
                                          self.subject_id,
@@ -154,5 +152,5 @@ class TestMapper:
         """ Long-running test. Do not run every time. """
         subject = "R1291M_1"
         mapper.build_prior_stim_location_mapping(self.subject_id,
-                                                 self.paths.basedir,
+                                                 self.paths.base,
                                                  "/data10/RAM/subjects/{}/imaging/autoloc/".format(subject))
